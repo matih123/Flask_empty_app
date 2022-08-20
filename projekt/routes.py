778ee5a9fb622331b83f5e6bf1fcf29a8b82cf3f
Mateusz Hojda <mati.hojda@gmail.com>
2022-08-20 22:51:18 +0200

    commit nazwa

--- projekt/routes.py
+++ projekt/routes.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
+
+@app.route('/login', methods=['GET, POST'])
+def login():
+    return render_template('login.html')
